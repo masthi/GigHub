414ed9c814ef85099060682bf4472ff3713e1b19
Masthan Rao <masthan.rao@outlook.com>
2019-09-29 19:15:22 +0000

    Added a GigForm

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using GigHub.Models;
+using System.Web.Mvc;
+using GigHub.ViewModels;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+                _context = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
