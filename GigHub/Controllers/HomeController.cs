4dac3d4f94346aa9005aeb2dcf2640e05678d19f
Masthan Rao <masthan.rao@outlook.com>
2019-10-20 18:24:26 +0000

    Moved all viewModels to respective folders

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GigHub.Models;
+using System.Data.Entity;
 
 namespace GigHub.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs
+                .Include((g => g.Artist))
+                .Where((g => g.DateTime > DateTime.Now));
+            return View(upcomingGigs);
         }
 
         public ActionResult About()
